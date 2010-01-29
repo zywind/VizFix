a14aa1ffce34fb3b8d139ad60899623258850312
YZ@CMET <zywind@cs.uoregon.edu>
2010-01-29 10:38:58 -0800

    Forgot to add two files in the last commit.

---
+++ VizFix/VFVisualAngleConverter.h
@@ -0,0 +1,40 @@
+//
+//  VFVisualAngleConverter.h
+//  VizFix
+//
+//  Created by Yunfeng Zhang on 1/28/10.
+//  Copyright 2010 University of Oregon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface VFVisualAngleConverter : NSObject {
+	NSSize screenResolution;
+	NSSize screenDimension;
+	NSUInteger distanceToScreen; 
+}
+
+/*!
+ In pixels.
+ */
+@property (assign) NSSize screenResolution;
+/*!
+ In mm.
+ */
+@property (assign) NSSize screenDimension;
+/*!
+ In mm.
+ */
+@property (assign) NSUInteger distanceToScreen;
+
+- (id)initWithDistanceToScreen:(NSUInteger)distance 
+			  screenResolution:(NSSize)resolution 
+			   screenDimension:(NSSize)dimension;
+
+- (double)horizontalPixelsFromVisualAngles:(double)DOV;
+- (double)horizontalVisualAnglesFromPixels:(double)pixels;
+- (double)verticalPixelsFromVisualAngles:(double)DOV;
+- (double)verticalVisualAnglesFromPixels:(double)pixels;
+
+@end
---
+++ VizFix/VFVisualAngleConverter.m
@@ -0,0 +1,54 @@
+//
+//  VFVisualAngleConverter.m
+//  VizFix
+//
+//  Created by Yunfeng Zhang on 1/28/10.
+//  Copyright 2010 University of Oregon. All rights reserved.
+//
+
+#import "VFVisualAngleConverter.h"
+
+
+@implementation VFVisualAngleConverter
+
+@synthesize screenResolution;
+@synthesize screenDimension;
+@synthesize distanceToScreen;
+
+#define RADIANS( degrees ) ( degrees * M_PI / 180 )
+#define DEGREES( radians) (radians * 180 / M_PI)
+
+- (id)initWithDistanceToScreen:(NSUInteger)distance 
+			  screenResolution:(NSSize)resolution 
+			   screenDimension:(NSSize)dimension
+{
+	self = [super init];
+    if (self != nil) {
+		self.distanceToScreen = distance;
+		self.screenResolution = resolution;
+		self.screenDimension = dimension;
+    }
+    return self;
+}
+
+- (double)horizontalPixelsFromVisualAngles:(double)DOV
+{
+	return tan(RADIANS(DOV/2))*2*distanceToScreen*screenResolution.width/screenDimension.width;
+}
+
+- (double)horizontalVisualAnglesFromPixels:(double)pixels
+{
+	return DEGREES(atan(((pixels/2)/(distanceToScreen*screenResolution.width/screenDimension.width))))*2;
+}
+
+- (double)verticalPixelsFromVisualAngles:(double)DOV
+{
+	return tan(RADIANS(DOV/2))*2*distanceToScreen*screenResolution.height/screenDimension.height;
+}
+
+- (double)verticalVisualAnglesFromPixels:(double)pixels
+{
+	return DEGREES(atan(((pixels/2)/(distanceToScreen*screenResolution.height/screenDimension.height))))*2;
+}
+
+@end
